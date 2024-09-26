8ada7fc5f1a93996863fd16d0fec95c51592955d
Daniel Mashuda <danmashuda@gmail.com>
2024-09-26 13:47:52 -0400

    Add rabbit config

--- AspireTestBed/Program.cs
+++ AspireTestBed/Program.cs
@@ -1,3 +1,13 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+
+var rabbit = builder.AddRabbitMQ("RabbitServer");
+
+builder.AddProject<Projects.CustomerApi>("CustomerApi")
+    .WithReference(rabbit);
+
+builder.AddProject<Projects.WebhookSender>("WebhookSender")
+    .WithReference(rabbit);
+
+
 builder.Build().Run();
